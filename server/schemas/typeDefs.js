066b3f6700b9d71ceb77e842c74df517686b55f2
Sephadeus <90430093+Sephadeus@users.noreply.github.com>
2023-03-18 20:10:28 -0400

    reconfigured savedBooks and searchBooks to use graphQL mutations and queries

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -40,7 +40,7 @@ const typeDefs = gql`
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
     saveBook(bookData: BookInput!): User
-    deleteBook(bookId: ID!): User
+    removeBook(bookId: ID!): User
   }
 `;
 
